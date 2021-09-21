21ec9f3cf1a5f86fce21cdb1296795870cc04c92
rijalagusr131 <rijalrusmana131@gmail.com>
2021-09-21 20:17:24 +0700

    Commit5
    Commit5

---
+++ Assets/Scripts/Back.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour
+{
+    public void LevelBefore()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
---
+++ Assets/explosive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosive : MonoBehaviour
+{
+    public GameObject ExplosionEffect;
+
+    void OnTriggerEnter2D ()
+    {
+        Instantiate(ExplosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
